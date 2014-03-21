375c854ad040c0b0353d5ca3a75bbc01fcb155a1
ryan <bli2@clemson.edu>
2014-03-21 15:04:42 -0400

    fix bug return from tablecell.

---
+++ Strawberry/infoViewController.h
@@ -0,0 +1,14 @@
+//
+//  infoViewController.h
+//  Strawberry
+//
+//  Created by Ted Lee on 3/21/14.
+//  Copyright (c) 2014 Ted Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface infoViewController : UIViewController
+- (IBAction)backButton:(id)sender;
+
+@end
---
+++ Strawberry/infoViewController.m
@@ -0,0 +1,41 @@
+//
+//  infoViewController.m
+//  Strawberry
+//
+//  Created by Ted Lee on 3/21/14.
+//  Copyright (c) 2014 Ted Lee. All rights reserved.
+//
+
+#import "infoViewController.h"
+
+@interface infoViewController ()
+
+@end
+
+@implementation infoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backButton:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+@end
